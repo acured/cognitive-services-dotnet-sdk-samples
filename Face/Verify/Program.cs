83887816a5226811d1e789e33c9d4c818393e6f0
Your Name <you@example.com>
2018-10-17 15:21:00 +0800

    add verify

---
+++ Face/Verify/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+
+namespace Verify
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create a client.
+            string apiKey = "793315696cd04d5ab78232a792136610";
+            IFaceClient client = new FaceClient(new ApiKeyServiceClientCredentials(apiKey))
+            {
+                Endpoint = "https://westus.api.cognitive.microsoft.com"
+            };
+        }
+    }
+}
