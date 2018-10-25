c46dd5fc18d4ba66bc5d58d2606b3c272ee45f0a
Your Name <you@example.com>
2018-10-25 16:59:14 +0800

    Add face test samples

---
+++ samples/Face/Group.cs
@@ -0,0 +1,64 @@
+﻿namespace Microsoft.Azure.CognitiveServices.Samples.Face
+{
+    using Microsoft.Azure.CognitiveServices.Vision.Face;
+    using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public static class Group
+    {
+        public static async Task Run(string endpoint, string key)
+        {
+            Console.WriteLine("Sample of grouping faces.");
+
+            IFaceClient client = new FaceClient(new ApiKeyServiceClientCredentials(key))
+            {
+                Endpoint = endpoint
+            };
+
+            List<string> imageFileNames =
+                new List<string> { "Family1-Dad1.jpg", "Family1-Dad2.jpg", "Family3-Lady1.jpg", "Family1-Daughter1.jpg", "Family1-Daughter2.jpg", "Family1-Daughter3.jpg" };
+            Dictionary<string, string> faces = new Dictionary<string, string>();
+            List<Guid> faceIds = new List<Guid>();
+
+            foreach (var imageFileName in imageFileNames)
+            {
+                // Detect faces from image file.
+                IList<DetectedFace> detectedFaces = await Common.DetectedFace(client, Path.Combine("Images", imageFileName));
+
+                // Add detected faceId to faceIds and faces.
+                faceIds.Add(detectedFaces[0].FaceId.Value);
+                faces.Add(detectedFaces[0].FaceId.ToString(), imageFileName);
+            }
+
+            // Call grouping, the grouping result is a group collection, each group contains similar faces.
+            var groupResult = await client.Face.GroupAsync(faceIds);
+
+            // Grouping results.
+            for (int i = 0; i < groupResult.Groups.Count; i++)
+            {
+                Console.Write($"Find face group {i + 1}: ");
+                foreach (var faceId in groupResult.Groups[i])
+                {
+                    Console.Write($"{faces[faceId.ToString()]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+            // MessyGroup contains all faces which are not similar to any other faces.
+            if (groupResult.MessyGroup.Count > 0)
+            {
+                Console.Write("Find messy face group: ");
+                foreach (var faceId in groupResult.MessyGroup)
+                {
+                    Console.Write($"{faces[faceId.ToString()]} ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
--- samples/Face/Program.cs
+++ samples/Face/Program.cs
@@ -7,10 +7,25 @@
         static void Main(string[] args)
         {
             // Create a client.
-            string apiKey = "ENTER YOUR KEY HERE";
-            string endpoint = "ENTER YOUR ENDPOINT HERE";
+            //string apiKey = "ENTER YOUR KEY HERE";
+            //string endpoint = "ENTER YOUR ENDPOINT HERE";
+            string apiKey = "793315696cd04d5ab78232a792136610";
+            string endpoint = "https://westus.api.cognitive.microsoft.com";
 
             VerifyFaceToFace.Run(endpoint, apiKey).Wait();
+            VerifyInPersonGroup.Run(endpoint, apiKey).Wait();
+            VerifyInLargePersonGroup.Run(endpoint, apiKey).Wait();
+
+            Detection.Run(endpoint, apiKey).Wait();
+
+            FindSimilarInFaceIds.Run(endpoint, apiKey).Wait();
+            FindSimilarInFaceList.Run(endpoint, apiKey).Wait();
+            FindSimilarInLargeFaceList.Run(endpoint, apiKey).Wait();
+
+            Group.Run(endpoint, apiKey).Wait();
+
+            IdentifyInPersonGroup.Run(endpoint, apiKey).Wait();
+            IdentifyInLargePersonGroup.Run(endpoint, apiKey).Wait();
 
             Console.WriteLine("\nPress ENTER to exit.");
             Console.ReadLine();
